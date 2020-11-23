c4b849384a3f0d194c2bcac0f750ccba6e397850
Roman <valeroman@gmail.com>
2020-11-23 07:43:00 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,49 @@
+// Servidor de Express
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+
+const Sockets = require('./sockets');
+
+
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer(this.app);
+
+        //Configuraciones del sockets
+        this.io = socketio(this.server, { /* configuraciones */ });
+    }
+
+    middlewares() {
+        // Desplegar directorio publico
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+    }
+
+    configurarSockets() {
+        new Sockets(this.io);
+    };
+
+    execute() {
+
+        // Inicializar middlewares
+        this.middlewares();
+
+        // Inicializar sockets
+        this.configurarSockets();
+
+        // Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en el puerto:', this.port);
+        });
+    }
+}
+
+module.exports = Server;
