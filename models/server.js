ea2507280660f10655e00cdba4c1f8be8ef192dd
Roman <valeroman@gmail.com>
2020-11-23 08:07:06 -0500

    habilitar cors

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -25,6 +26,9 @@ class Server {
     middlewares() {
         // Desplegar directorio publico
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        // CORS
+        this.app.use(cors());
     }
 
     configurarSockets() {
